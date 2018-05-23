09c6a37dee00c9e4d2d0d9b8bb4283c69a92d9bd
Aki Häkkilä <3113587+ahakkila@users.noreply.github.com>
2018-05-23 15:24:26 +0300

    add package documentation, remove exporting of Logger struct fields

--- logger.go
+++ logger.go
@@ -1,3 +1,4 @@
+// Package logger provides functionality for logging on various loglevels
 package logger
 
 import (
@@ -10,152 +11,177 @@ import (
 )
 
 type Logger struct {
-	CountLines  bool
-	CountChars  bool
-	FatalWriter io.Writer
-	ErrorWriter io.Writer
-	WarnWriter  io.Writer
-	InfoWriter  io.Writer
-	TraceWriter io.Writer
+	countLines  bool
+	countChars  bool
+	fatalWriter io.Writer
+	errorWriter io.Writer
+	warnWriter  io.Writer
+	infoWriter  io.Writer
+	traceWriter io.Writer
 }
 
+// Create a new logger.Logger
 func NewLogger() *Logger {
 	return &Logger{
-		CountLines:  false,
-		CountChars:  false,
-		FatalWriter: ioutil.Discard,
-		ErrorWriter: ioutil.Discard,
-		WarnWriter:  ioutil.Discard,
-		InfoWriter:  ioutil.Discard,
-		TraceWriter: ioutil.Discard,
+		countLines:  false,
+		countChars:  false,
+		fatalWriter: ioutil.Discard,
+		errorWriter: ioutil.Discard,
+		warnWriter:  ioutil.Discard,
+		infoWriter:  ioutil.Discard,
+		traceWriter: ioutil.Discard,
 	}
 }
 
+// Redirect all output to stdout
 func (l *Logger) AllStdout() *Logger {
-	l.FatalWriter = os.Stdout
-	l.ErrorWriter = os.Stdout
-	l.WarnWriter = os.Stdout
-	l.InfoWriter = os.Stdout
-	l.TraceWriter = os.Stdout
+	l.fatalWriter = os.Stdout
+	l.errorWriter = os.Stdout
+	l.warnWriter = os.Stdout
+	l.infoWriter = os.Stdout
+	l.traceWriter = os.Stdout
 	return l
 }
 
+// Redirect Fatal loglevel to stdout
 func (l *Logger) FatalStdout() *Logger {
 	return l.FatalOutput(os.Stdout)
 }
 
+// Redirect Error loglevel to stdout
 func (l *Logger) ErrorStdout() *Logger {
 	return l.ErrorOutput(os.Stdout)
 }
 
+// Redirect Warning loglevel to stdout
 func (l *Logger) WarnStdout() *Logger {
 	return l.WarnOutput(os.Stdout)
 }
 
+// Redirect Information loglevel to stdout
 func (l *Logger) InfoStdout() *Logger {
 	return l.InfoOutput(os.Stdout)
 }
 
+// Redirect Trace loglevel to stdout
 func (l *Logger) TraceStdout() *Logger {
 	return l.TraceOutput(os.Stdout)
 }
 
+// Discard all output, redirected to ioutil.Discard
 func (l *Logger) DiscardAll() *Logger {
-	l.FatalWriter = ioutil.Discard
-	l.ErrorWriter = ioutil.Discard
-	l.WarnWriter = ioutil.Discard
-	l.InfoWriter = ioutil.Discard
-	l.TraceWriter = ioutil.Discard
+	l.fatalWriter = ioutil.Discard
+	l.errorWriter = ioutil.Discard
+	l.warnWriter = ioutil.Discard
+	l.infoWriter = ioutil.Discard
+	l.traceWriter = ioutil.Discard
 	return l
 }
 
+// Discard Fatal output, redirected to ioutil.Discard
 func (l *Logger) FatalDiscard() *Logger {
 	return l.FatalOutput(ioutil.Discard)
 }
 
+// Discard Error output, redirected to ioutil.Discard
 func (l *Logger) ErrorDiscard() *Logger {
 	return l.ErrorOutput(ioutil.Discard)
 }
 
+// Discard Warning output, redirected to ioutil.Discard
 func (l *Logger) WarnDiscard() *Logger {
 	return l.WarnOutput(ioutil.Discard)
 }
 
+// Discard Information output, redirected to ioutil.Discard
 func (l *Logger) InfoDiscard() *Logger {
 	return l.InfoOutput(ioutil.Discard)
 }
 
+// Discard Trace output, redirected to ioutil.Discard
 func (l *Logger) TraceDiscard() *Logger {
 	return l.TraceOutput(ioutil.Discard)
 }
 
+// Redirect all output to w
 func (l *Logger) AllOutput(w io.Writer) *Logger {
-	l.FatalWriter = w
-	l.ErrorWriter = w
-	l.WarnWriter = w
-	l.InfoWriter = w
-	l.TraceWriter = w
+	l.fatalWriter = w
+	l.errorWriter = w
+	l.warnWriter = w
+	l.infoWriter = w
+	l.traceWriter = w
 	return l
 }
 
+// Redirect Fatal output to w
 func (l *Logger) FatalOutput(w io.Writer) *Logger {
-	l.FatalWriter = w
+	l.fatalWriter = w
 	return l
 }
 
+// Redirect Error output to w
 func (l *Logger) ErrorOutput(w io.Writer) *Logger {
-	l.ErrorWriter = w
+	l.errorWriter = w
 	return l
 }
 
+// Redirect Warning output to w
 func (l *Logger) WarnOutput(w io.Writer) *Logger {
-	l.WarnWriter = w
+	l.warnWriter = w
 	return l
 }
 
+// Redirect Information output to w
 func (l *Logger) InfoOutput(w io.Writer) *Logger {
-	l.InfoWriter = w
+	l.infoWriter = w
 	return l
 }
 
+// Redirect Trace output to w
 func (l *Logger) TraceOutput(w io.Writer) *Logger {
-	l.TraceWriter = w
+	l.traceWriter = w
 	return l
 }
 
+// Log a message on Fatal loglevel and call panic()
 func (l *Logger) Fatal(format string, a ...interface{}) {
 	f := FNameForLogger()
 	line := fmt.Sprintf(format, a...)
-	panicline := fmt.Sprintf(" [%s] %s (fatal): %s", l.Now(), f, line)
-	fmt.Fprintln(l.FatalWriter, panicline)
+	panicline := fmt.Sprintf(" [%s] %s (fatal): %s", now(), f, line)
+	fmt.Fprintln(l.fatalWriter, panicline)
 	panic(panicline)
 }
 
+// Log a message on Error loglevel
 func (l *Logger) Error(format string, a ...interface{}) {
 	f := FNameForLogger()
 	line := fmt.Sprintf(format, a...)
-	fmt.Fprintf(l.ErrorWriter, " [%s] %s (error): %s\n", l.Now(), f, line)
+	fmt.Fprintf(l.errorWriter, " [%s] %s (error): %s\n", now(), f, line)
 }
 
+// Log a message on Warning loglevel
 func (l *Logger) Warn(format string, a ...interface{}) {
 	f := FNameForLogger()
 	line := fmt.Sprintf(format, a...)
-	fmt.Fprintf(l.WarnWriter, " [%s] %s (warn): %s\n", l.Now(), f, line)
+	fmt.Fprintf(l.warnWriter, " [%s] %s (warn): %s\n", now(), f, line)
 }
 
+// Log a message on Information loglevel
 func (l *Logger) Info(format string, a ...interface{}) {
 	f := FNameForLogger()
 	line := fmt.Sprintf(format, a...)
-	fmt.Fprintf(l.InfoWriter, " [%s] %s (info): %s\n", l.Now(), f, line)
+	fmt.Fprintf(l.infoWriter, " [%s] %s (info): %s\n", now(), f, line)
 }
 
+// Log a message on Trace loglevel
 func (l *Logger) Trace(format string, a ...interface{}) {
 	f := FNameForLogger()
 	line := fmt.Sprintf(format, a...)
-	fmt.Fprintf(l.TraceWriter, " [%s] %s (trace): %s\n", l.Now(), f, line)
+	fmt.Fprintf(l.traceWriter, " [%s] %s (trace): %s\n", now(), f, line)
 }
 
-func (l *Logger) Now() string {
+// Return timestring in "15:04:05" format
+func now() string {
 	return time.Now().Format("15:04:05")
 }
 
